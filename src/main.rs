ace4ea26a3ad0797c7f0bd138b2850048e45b71e
vinay-04 <vinayrajan04@gmail.com>
2024-04-27 17:13:57 +0530

    feat: getData with rss

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 mod news_scraper;
 
 fn main() {
-    let news = news_scraper::news_data_scraper();
-    for n in news {
-        println!("Title: {}", n.title.unwrap());
-        println!("Time: {}", n.time.unwrap());
-        println!("URL: {}", n.url.unwrap());
-        println!();
+    let news = news_scraper::news_data_rss();
+    for n in news.iter() {
+        println!("{:?}", n.title);
     }
 }
--- src/news_scraper.rs
+++ src/news_scraper.rs
@@ -1,39 +1,73 @@
 use reqwest::blocking;
-use scraper::{self, selectable::Selectable};
+use rss::Channel;
 
 pub struct News {
     pub title: Option<String>,
     pub time: Option<String>,
     pub url: Option<String>,
+    pub description: Option<String>,
 }
 
-pub fn news_data_scraper() -> Vec<News> {
-    let url = "https://www.moneycontrol.com/news/business/stocks/";
+// --------------------------------------------------------------------------------------   WEBSCRAPED DATA FROM MONEYCONTROL.COM  --------------------------------------------------------------------------------------
+// pub fn news_data_scraper() -> Vec<News> {
+//     let url = "https://www.moneycontrol.com/news/business/stocks/";
+//     let data = blocking::get(url).unwrap().text().unwrap();
+//     let document = scraper::Html::parse_document(&data);
+
+//     let mut news: Vec<News> = Vec::new();
+
+//     let html_product_selector = scraper::Selector::parse("li.clearfix").unwrap();
+//     let html_products = document.select(&html_product_selector);
+
+//     for product in html_products {
+//         let url = product
+//             .select(&scraper::Selector::parse("a").unwrap())
+//             .next()
+//             .and_then(|a| a.value().attr("href"))
+//             .map(str::to_owned);
+//         let title = product
+//             .select(&scraper::Selector::parse("a").unwrap())
+//             .next()
+//             .and_then(|a| a.value().attr("title"))
+//             .map(str::to_owned);
+//         let time = product
+//             .select(&scraper::Selector::parse("span").unwrap())
+//             .next()
+//             .and_then(|span| span.text().next())
+//             .map(str::to_owned);
+//         let description = product
+//             .select(&scraper::Selector::parse("p").unwrap())
+//             .next()
+//             .and_then(|p| p.text().next())
+//             .map(str::to_owned);
+//         let news_data = News {
+//             title,
+//             time,
+//             url,
+//             description,
+//         };
+//         news.push(news_data);
+//     }
+//     return news;
+// }
+// --------------------------------------------------------------------------------------   XXXXXXXXXXXXXXXXXXXXX  --------------------------------------------------------------------------------------
+
+pub fn news_data_rss() -> Vec<News> {
+    let url = "https://www.moneycontrol.com/rss/latestnews.xml";
     let data = blocking::get(url).unwrap().text().unwrap();
-    let document = scraper::Html::parse_document(&data);
-
+    let channel = Channel::read_from(data.as_bytes()).unwrap();
     let mut news: Vec<News> = Vec::new();
-
-    let html_product_selector = scraper::Selector::parse("li.clearfix").unwrap();
-    let html_products = document.select(&html_product_selector);
-
-    for product in html_products {
-        let url = product
-            .select(&scraper::Selector::parse("a").unwrap())
-            .next()
-            .and_then(|a| a.value().attr("href"))
-            .map(str::to_owned);
-        let title = product
-            .select(&scraper::Selector::parse("a").unwrap())
-            .next()
-            .and_then(|a| a.value().attr("title"))
-            .map(str::to_owned);
-        let time = product
-            .select(&scraper::Selector::parse("span").unwrap())
-            .next()
-            .and_then(|span| span.text().next())
-            .map(str::to_owned);
-        let news_data = News { title, time, url };
+    for item in channel.items {
+        let title = item.title.unwrap();
+        let url = item.link.unwrap();
+        let time = item.pub_date.unwrap();
+        let description = item.description.unwrap();
+        let news_data = News {
+            title: Some(title),
+            time: Some(time),
+            url: Some(url),
+            description: Some(description),
+        };
         news.push(news_data);
     }
     return news;
