4452f0819e67e0d41db016a8b4e04f39b38c6ef5
vinay-04 <vinayrajan04@gmail.com>
2024-04-29 20:15:57 +0530

    styling ig

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import news from "../src-tauri/newsData.json";
+import { invoke } from "@tauri-apps/api";
 import "./App.css";
 
-function App() {
 
+function App() {
 
   return (
     <div class="container">
       <h1>Mews!!</h1>
       <h2>Get latest updates of Market News.</h2>
-      <h3></h3>
 
-      <p>Click on the News to learn more.</p>
+      <h3>Click on the News to learn more.</h3>
 
-      <p>
+
   {news.map((item) => (
-    <p>{item.title}</p>
+    <a href={`${item.url}`}>{item.title}</a>
   ))}
-</p>
+
     </div>
   );
 }
